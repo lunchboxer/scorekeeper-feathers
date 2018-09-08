cd51f26bd7557162d5c25d2e63e659fc66bec858
James <metal.lunchbox@gmail.com>
2018-09-08 21:56:40 +0800

    removed validator, uneeded and less explicit than plain js

--- src/hooks/check-birthdate.js
+++ src/hooks/check-birthdate.js
@@ -1,11 +1,13 @@
-const isAfter = require('validator/lib/isAfter')
-
 // eslint-disable-next-line no-unused-vars
 module.exports = function () {
   return context => {
     const { birthdate } = context.data
-    if (birthdate && isAfter(birthdate)) {
-      throw new Error('Birth date must be a past date.')
+    if (birthdate) {
+      const bdate = new Date(birthdate)
+      const now = new Date()
+      if (bdate > now) {
+        throw new Error('Birth date must be a past date.')
+      }
     }
     return context
   }
--- src/hooks/check-session-dates-within-year.js
+++ src/hooks/check-session-dates-within-year.js
@@ -1,4 +1,3 @@
-const isAfter = require('validator/lib/isAfter')
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
@@ -8,14 +7,19 @@ module.exports = function () {
     const { startsAt, endsAt } = context.data
     // if neither time is being worked on then skip ahead
     if (startsAt || endsAt) {
-      // first make sure the dates are within a year from now
       const now = new Date()
       const aYearFromNow = new Date(now.valueOf() + 3.154e10).toJSON()
-      if (startsAt && isAfter(startsAt, aYearFromNow)) {
-        throw new Error('Start time cannot be more than a year ahead.')
+      if (startsAt) {
+        const startsAtDate = new Date(startsAt)
+        if (startsAtDate > aYearFromNow) {
+          throw new Error('Start time cannot be more than a year ahead.')
+        }
       }
-      if (endsAt && isAfter(endsAt, aYearFromNow)) {
-        throw new Error('End time cannot be more than a year ahead.')
+      if (endsAt) {
+        const endsAtDate = new Date(endsAt)
+        if (endsAtDate > aYearFromNow) {
+          throw new Error('End time cannot be more than a year ahead.')
+        }
       }
     }
     return context
