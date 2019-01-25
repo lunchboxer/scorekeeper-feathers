40af60d71fab567821bfa13dab459157e890afd5
James <metal.lunchbox@gmail.com>
2019-01-25 10:03:37 +0800

    simplify service call in limit-items

--- src/hooks/limit-items.js
+++ src/hooks/limit-items.js
@@ -4,12 +4,12 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = function (limit = 100) {
   return async context => {
-    const items = await context.app
-      .service(context.path)
-      .find({ query: { $limit: 1, $skip: limit, $sort: { createdAt: -1 } } })
+    const items = await context.service.find({
+      query: { $limit: 1, $skip: limit, $sort: { createdAt: -1 } }
+    })
     if (items.total <= limit) return context
     const oneExtra = items.data[0]
-    context.app.service(context.path).remove(oneExtra.id)
+    context.service.remove(oneExtra.id)
     return context
   }
 }
